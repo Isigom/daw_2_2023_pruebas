dab1b94801e38ec8c0d90c542a01f31d5796bd72
isigom <imanol.s334306@cesurformacion.com>
2023-10-21 04:34:16 +0200

    inicio de proyecto de prueba

---
+++ hola.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Primer php</title>
+  </head>
+  <body>
+    <?php echo "Esta función es echo sirve para ver lo que queremos mostrar por pantalla" ?>
+  </body>
+</html>
